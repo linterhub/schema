b9c61ea78d089da135a4aa211390073a1aa9b7a0
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-06-14 16:57:57 +0300

    feat: add a command to update license.spdx and language.linguist schemas
    added next gulp commands:
    - `import-licenses` - import licenses of spdx to `license.spdx` schema
    - `import-languages` - import languages of linguist to `language.linguist` schema
    - `pull-submodules` - update git submodules `spdx` and `linguist`
    
    added gulp series commands:
    - `import` - included: `pull-submodules`, `import-licenses` and `import-languages`
    
    added npm command:
    - `import` - it's command `gulp import`
    
    Closes #11

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// Set global shared core
+const lhcore = require('./script/gulp/index.js');
+global.lhcore = lhcore;
+
+// External modules as aliases
+const gulp = lhcore.amd.gulp;
+const hubRegistry = lhcore.amd.hubRegistry;
+
+// Load tasks into the registry of gulp
+hubRegistry([
+    'script/gulp/**/task.*.js',
+]);
+
+gulp.task('default', gulp.series('import'));
+gulp.task('import', gulp.series('pull-submodules', 'import'));
