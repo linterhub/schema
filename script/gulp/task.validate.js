454b736ae6c682db139e712e034f0d5f272dbb71
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-06-14 18:27:15 +0300

    feat: add a command to validate schemas
    added next gulp command:
    - `validate-core` - validate core schemas in folder `src/schema`
    - `validate-collection`  - validate all collections schemas in folder `src/collection`
    
    added next series command:
    - `validate` - included: `import`, 'validate-core` and `validate-collection` commands
    
    Closes #13

---
+++ script/gulp/task.validate.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Get shared core
+const core = global.lhcore;
+
+// External modules as aliases
+const gulp = core.amd.gulp;
+const jsonSchema = core.amd.jsonSchema;
+const config = core.cfg;
+
+// Validate all core schemas according to schemaver
+const validateCore = () => gulp
+    .src(config.schema.mask)
+    .pipe(jsonSchema({
+        schema: config.schema.ver,
+        loadMissingSchemas: true,
+        checkRecursive: true,
+        verbose: true,
+    }));
+
+// Validate all collection schemas according to schemaver
+const validateCollection = () => gulp
+    .src(config.collection.mask)
+    .pipe(jsonSchema({
+        schema: config.schema.draft,
+        loadMissingSchemas: true,
+        checkRecursive: true,
+        verbose: true,
+    }));
+
+// Tasks
+gulp.task('validate-core', validateCore);
+gulp.task('validate-collection', validateCollection);
+gulp.task('validate', gulp.parallel('validate-core', 'validate-collection'));
