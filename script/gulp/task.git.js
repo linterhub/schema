d786980cfd85da75f722823249a033fc40a74492
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-06-15 17:15:27 +0300

    ci: add semantic release
    added semantic release to a project, include:
    - changelog
    - release to GitHub
    - automatic update version in core schemas
    - automatic update version in package.json
    
    Closes #30

--- script/gulp/task.git.js
+++ script/gulp/task.git.js
@@ -10,5 +10,22 @@ const git = core.amd.git;
 // Update git submodules
 const pullSubmodules = () => git.updateSubmodule({args: '--init'});
 
+// Remote and merge git submodules
+const mergeSubmodules = () => git.updateSubmodule({args: '--remote --merge'});
+
+// Git fetch
+const fetch = () => git.exec({args: 'fetch'});
+
+// Git rebase
+const rebase = () => git.exec({args: 'rebase'});
+
 // Tasks
+gulp.task('fetch', fetch);
+gulp.task('rebase', rebase);
+gulp.task('merge-submodules', mergeSubmodules);
 gulp.task('pull-submodules', pullSubmodules);
+
+gulp.task('update-submodules', gulp.series(
+            'fetch',
+            'rebase',
+            'merge-submodules'));
