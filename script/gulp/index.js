9a076dd939ddddb8ec3d69442cfc6132278c5cc3
xferra <xferra@live.com>
2018-06-15 06:15:32 +0300

    ci: add spellcheck and common linting
    Add recommended vscode extensions and lint rules
    
    Closes #26

--- script/gulp/index.js
+++ script/gulp/index.js
@@ -1,32 +1,34 @@
-// Shared conffguration
+'use strict';
+
+// Shared configuration
 const cfg = require('./config.json');
 
 // Shared node modules
 const amd = {
-    fs: require('fs'),
-    log: require('fancy-log'),
-    git: require('gulp-git'),
-    yaml: require('js-yaml'),
-    eslint: require('gulp-eslint'),
-    gulp: require('gulp'),
-    hubRegistry: require('gulp-hub'),
-    jsonData: require('gulp-data'),
-    jsonFormat: require('gulp-json-format'),
-    jsonSchema: require('gulp-json-schema'),
-    jsonSchemaBundle: require('gulp-jsonschema-bundle'),
-    jsonSchemaValidator: require('jsonschema').Validator,
+  fs: require('fs'),
+  log: require('fancy-log'),
+  git: require('gulp-git'),
+  yaml: require('js-yaml'),
+  eslint: require('gulp-eslint'),
+  gulp: require('gulp'),
+  hubRegistry: require('gulp-hub'),
+  jsonData: require('gulp-data'),
+  jsonFormat: require('gulp-json-format'),
+  jsonSchema: require('gulp-json-schema'),
+  jsonSchemaBundle: require('gulp-jsonschema-bundle'),
+  jsonSchemaValidator: require('jsonschema').Validator,
 };
 
 // Shared functions
 const fnc = {
-    readJson: (path) => JSON.parse(amd.fs.readFileSync(path)),
-    readYaml: (path) => amd.yaml.safeLoad(amd.fs.readFileSync(path)),
-    jsonToBuffer: (json) => Buffer.from(JSON.stringify(json), 'utf8'),
+  readJson: (path) => JSON.parse(amd.fs.readFileSync(path)),
+  readYaml: (path) => amd.yaml.safeLoad(amd.fs.readFileSync(path)),
+  jsonToBuffer: (json) => Buffer.from(JSON.stringify(json), 'utf8'),
 };
 
-// Exported shared config, modules and functons
+// Exported shared config, modules and functions
 exports = module.exports = {
-    cfg: cfg,
-    amd: amd,
-    fnc: fnc,
+  cfg: cfg,
+  amd: amd,
+  fnc: fnc,
 };
