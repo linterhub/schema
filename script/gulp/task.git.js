330515fe54376252303c7179aae0281f8db30a54
xferra <xferra@live.com>
2018-06-17 18:23:23 +0300

    ci: make gulp task names unique
    Improve naming and ordering
    
    Closes #37

--- script/gulp/task.git.js
+++ script/gulp/task.git.js
@@ -8,7 +8,17 @@ const gulp = core.amd.gulp;
 const git = core.amd.git;
 
 // Update git submodules
-const pullSubmodules = () => git.updateSubmodule({args: '--init'});
+const pullSubmodules = () => git.updateSubmodule({
+    args: '--init --remote --merge',
+});
+
+// Pull git repository
+const pullRepository = () => git.pull({
+    args: '--rebase',
+});
+
 
 // Tasks
-gulp.task('pull-submodules', pullSubmodules);
+gulp.task('git-sub-pull', pullSubmodules);
+gulp.task('git-pull', pullRepository);
+gulp.task('pull', gulp.series('git-sub-pull', 'git-pull'));
