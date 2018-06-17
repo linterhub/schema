468db43874a5886cf96d3d96599aba0b9a7e5aae
xferra <xferra@live.com>
2018-06-17 23:30:46 +0300

    ci: add semantic release
    Stages:
    - auto update version
    - generate changelog
    - release to GitHub
    - release to npm registry
    - release to gh-pages
    
    Closes #30

--- script/gulp/task.release.js
+++ script/gulp/task.release.js
@@ -5,11 +5,16 @@ const core = global.lhcore;
 
 // External modules as aliases
 const gulp = core.amd.gulp;
+const jsonFormat = core.amd.jsonFormat;
+const jsonData = core.amd.jsonData;
 const path = core.amd.path;
+const log = core.amd.log;
 const config = core.cfg;
 
 // Externally functions as aliases
 const readJson = core.fnc.readJson;
+const jsonToBuffer = core.fnc.jsonToBuffer;
+const nextRelease = core.fnc.nextRelease;
 
 // Copy all static assets to release
 const copyStatic = () => gulp
@@ -19,6 +24,13 @@ const copyStatic = () => gulp
 // Copy to release all schemas and update root
 const createRelease = () => gulp
     .src(config.build.mask)
+    .pipe(jsonData((file) => {
+        let content = readJson(file.path);
+        content.$version = nextRelease();
+        file.contents = jsonToBuffer(content);
+        return file;
+    }))
+    .pipe(jsonFormat(4))
     .pipe(gulp.dest(config.release.dir))
     .pipe(gulp.dest((file) => {
         let content = readJson(file.path);
@@ -26,7 +38,13 @@ const createRelease = () => gulp
         return path.join(config.release.dir, version);
     }));
 
+// Log release number
+const logRelease = (done) => {
+    log.info(`Release: ${nextRelease()}`);
+    done();
+};
+
 // Tasks
 gulp.task('static', copyStatic);
 gulp.task('create-release', createRelease);
-gulp.task('release', gulp.parallel('static', 'create-release'));
+gulp.task('release', gulp.parallel('static', 'create-release', logRelease));
