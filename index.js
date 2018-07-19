a2d723b123bf76779627d7704f95444333aa975b
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-07-19 12:46:29 +0300

    ci: fix exports core schemas
    Fixed exports core schemas in `index.js` file
    
    Closes #158

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-exports = {
+exports = module.exports = {
     collection: require('./dist/collection.json'),
     deps: require('./dist/deps.json'),
     linter: require('./dist/linter.json'),
     package: require('./dist/package.json'),
     schemaver: require('./dist/schemaver.json'),
+    import: require('./dist/import.json'),
 };
